8d46075ba4ddbc2b63f5571e54e2af123d10ad1f
yurij <nabor_2008@i.ua>
2020-05-07 05:27:45 -0400

    current position complete

--- public_html/current-position.php
+++ public_html/current-position.php
@@ -36,9 +36,12 @@
                     let lat = position.coords.latitude;
                     let long = position.coords.longitude;
                     let time = position.timestamp;
+                    let longlat = lat + "," + long;
 
                     document.getElementById("result").innerHTML = "Latitude: " + lat + "<br> Longitude: " + long + "<br> Time: " + time;
+                    let mapUrl = "https://maps.googleapis.com/maps/api/staticmap?center=" + longlat + "&zoom=15&size=500x500&maptype=satellite&markers=color:red%7Clabel:I%7C" + longlat + "&key=AIzaSyAOj7WmwEcxrMnABjmJj5gecfI-wGwSiTo";
 
+                    document.getElementById("mycurrentposition").innerHTML = "<img src='" + mapUrl + "'>";
                     // alert("Accuracy: " + position.coords.accuracy);
                     // alert("Altitude: " + position.coords.altitude);
                     // alert("Altitude Accuracy: " + position.coords.altitudeAccuracy);
@@ -53,11 +56,28 @@
             </button>
 
         </div>
-
-
     </div>
 
+    <!--    <script>-->
+    <!--        window.onload = function () {-->
+    <!--            let lat = 48.379433;-->
+    <!--            let long = 31.16558;-->
+    <!--            let longlat = lat + "," + long;-->
+    <!---->
+    <!--            let mapUrl = "https://maps.googleapis.com/maps/api/staticmap?center=" + longlat + "&zoom=15&size=500x500&maptype=satellite&key=AIzaSyAOj7WmwEcxrMnABjmJj5gecfI-wGwSiTo";-->
+    <!---->
+    <!--            document.getElementById("mycurrentposition").innerHTML = "<img src='" + mapUrl + "'>";-->
+    <!--        }-->
+    <!--    </script>-->
+    <hr>
 
+    <div id="mycurrentposition" style="width: 500px; height: 500px;">
+        <!--        <img src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap-->
+        <!--&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318-->
+        <!--&markers=color:red%7Clabel:C%7C40.718217,-73.998284-->
+        <!--&key=AIzaSyAOj7WmwEcxrMnABjmJj5gecfI-wGwSiTo">-->
+    </div>
+    <hr>
 </div>
 </body>
 </html>
